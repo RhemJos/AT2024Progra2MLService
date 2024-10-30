5b7bc1cc6c93d008d30b17424d263177b2df5e35
Eynar92 <eynar.j.alvarez.m@gmail.com>
2024-10-30 14:15:43 -0400

    create the class UserController

---
+++ controllers/user_controller.py
@@ -0,0 +1,23 @@
+from services.user_service import UserService
+from flask import request
+
+user_service = UserService()
+
+
+class UserController:
+    @staticmethod
+    def create_user():
+        id = request.get_json("id")
+        name = request.get_json("name")
+        email = request.get_json("email")
+        password = request.get_json("password")
+        new_user = user_service.create_user(id, name, email, password)
+        return new_user.to_dict()
+
+    @staticmethod
+    def get_users():
+        return user_service.get_all_users()
+
+    @staticmethod
+    def get_user(user_id):
+        return user_service.get_user_by_id(user_id)
