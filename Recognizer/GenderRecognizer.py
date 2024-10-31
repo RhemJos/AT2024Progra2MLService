66b80fa22769f5c4ca54d859e9953782b46b3474
RhemHuarachi <rhemjos@gmail.com>
2024-10-31 18:58:14 -0400

    Copyright added and 'model' parameter removed

--- Recognizer/GenderRecognizer.py
+++ Recognizer/GenderRecognizer.py
@@ -1,5 +1,5 @@
 #
-# @video_converter.py Copyright (c) 2021 Jalasoft. # 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia. # <add direccion de jala la paz>
+# @GenderRecognizer.py Copyright (c) 2021 Jalasoft. # 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia. # <add direccion de jala la paz>
 # All rights reserved. #
 # This software is the confidential and proprietary information of
 # Jalasoft, ("Confidential Information"). You shall not
