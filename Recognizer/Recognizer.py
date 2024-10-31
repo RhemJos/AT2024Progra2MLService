d0487d91bd0ed0fc5562065f1fa8a404f013a0e3
RhemHuarachi <rhemjos@gmail.com>
2024-10-31 18:46:40 -0400

    updated folder

--- Recognizer/Recognizer.py
+++ Recognizer/Recognizer.py
@@ -1,8 +1,19 @@
+#
+# @video_converter.py Copyright (c) 2021 Jalasoft. # 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia. # <add direccion de jala la paz>
+# All rights reserved. #
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
 from abc import ABC, abstractmethod
 
 
 class Recognizer(ABC):
 
     @abstractmethod
-    def recognize(self, image_path: str, percentage: float = 0.1, word : str =None, model =None):
+    def recognize(self, image_path: str, confidence_threshold: float = 0.1, word: str = None):
         pass
+
